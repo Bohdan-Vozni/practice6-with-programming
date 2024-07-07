b1eef8a131b780c54f7d94525f66f635627c6c59
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2024-07-08 00:30:12 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+	char coordinates[20];
+	printf("Input circle coordinates: ");
+	fgets(coordinates, sizeof(coordinates), stdin);
+
+
+
+	char* next_token;
+	char* token = strtok_s(coordinates, " ", &next_token);
+	int count = 0;
+	float tokstring[7];
+	while (token != NULL) {
+		tokstring[count] = atof(token);
+		//printf("\ntoken %s", token);
+		token = strtok_s(NULL, " ", &next_token);
+		count++;
+
+		if (count > 4) {
+			printf("Erorr");
+			return 13;
+		}
+	}
+	float distance = sqrt(pow(tokstring[2] - tokstring[0], 2) + pow(tokstring[3] - tokstring[1], 2));
+	printf("\ndistance: %.6f", distance);
+
+	
+/*
+	for (int i = 0; i < count; i++) {	
+		printf("\nArray: %.2f", tokstring[i]);
+	}
+	*/
+
+	return 0;
+}
